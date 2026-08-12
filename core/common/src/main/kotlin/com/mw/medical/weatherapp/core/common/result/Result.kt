e0d971fea95884884e25e0fcb540f9580eea404d
Maksymilian Wojcik <maksymilian.wojcik@gmail.com>
2026-08-12 21:49:49 +0200

    Drop unused Result helpers

--- core/common/src/main/kotlin/com/mw/medical/weatherapp/core/common/result/Result.kt
+++ core/common/src/main/kotlin/com/mw/medical/weatherapp/core/common/result/Result.kt
@@ -6,10 +6,3 @@ sealed interface Result<out T> {
     data class Success<out T>(val value: T) : Result<T>
     data class Failure(val error: AppError) : Result<Nothing>
 }
-
-inline fun <T, R> Result<T>.map(transform: (T) -> R): Result<R> = when (this) {
-    is Result.Success -> Result.Success(transform(value))
-    is Result.Failure -> this
-}
-
-fun <T> Result<T>.getOrNull(): T? = (this as? Result.Success)?.value
