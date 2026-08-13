5544c228eae432b99812f660796caecb23ca61f9
Maksymilian Wojcik <maksymilian.wojcik@gmail.com>
2026-08-13 16:00:00 +0200

    Provide dispatchers from the app module

--- core/data/src/main/kotlin/com/mw/medical/weatherapp/core/data/di/DataModule.kt
+++ core/data/src/main/kotlin/com/mw/medical/weatherapp/core/data/di/DataModule.kt
@@ -1,14 +1,11 @@
 package com.mw.medical.weatherapp.core.data.di
 
-import com.mw.medical.weatherapp.core.common.dispatcher.DefaultDispatcherProvider
-import com.mw.medical.weatherapp.core.common.dispatcher.DispatcherProvider
 import com.mw.medical.weatherapp.core.data.repository.GeocodingRepositoryImpl
 import com.mw.medical.weatherapp.core.data.repository.WeatherRepositoryImpl
 import com.mw.medical.weatherapp.core.domain.repository.GeocodingRepository
 import com.mw.medical.weatherapp.core.domain.repository.WeatherRepository
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
@@ -24,12 +21,4 @@ internal abstract class DataModule {
     @Binds
     @Singleton
     abstract fun bindGeocodingRepository(impl: GeocodingRepositoryImpl): GeocodingRepository
-
-    companion object {
-        @Provides
-        @Singleton
-        fun provideDispatcherProvider(): DispatcherProvider {
-            return DefaultDispatcherProvider()
-        }
-    }
 }
